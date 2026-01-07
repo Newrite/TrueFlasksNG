a234b4fe988342be1a72d2041f8ab37273910d96
Newrite <newrite.work@yandex.ru>
2026-01-07 17:46:41 +0700

    Начало имплементации

--- src/Features/TrueFlasks.cpp
+++ src/Features/TrueFlasks.cpp
@@ -1,3 +1,19 @@
 export module TrueFlasks.Features.TrueFlasks;
 
-namespace features::true_flasks {}
+import TrueFlasks.Core.HooksCtx;
+import TrueFlasks.Config;
+import TrueFlasks.Core.ActorsCache;
+
+namespace features::true_flasks
+{
+
+bool drink_potion(core::hooks_ctx::on_actor_drink_potion& ctx)
+{
+  
+  const auto& actor_data = core::actors_cache::cache_data::get_singleton()->get_or_add(ctx.actor->GetFormID());
+  const auto config = config::config_manager::get_singleton();
+  
+  return true;
+}
+
+}
